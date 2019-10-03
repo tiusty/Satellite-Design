6dbca188cabc7b26501aa1870db5b8aa54f02ebc
Alex Agudelo <awagud12@gmail.com>
2019-10-02 21:47:42 -0400

    Added ground station systems

---
+++ gsReceiverSystem.m
@@ -0,0 +1,20 @@
+classdef gsReceiverSystem
+    % The ground station that receives the signal from the satellite
+   properties
+        % Phsyical properties of the system
+        Grant = 60; % The recieve antenna directivity in dB
+        
+        % Properties related to internal components
+        Tant = 190; % Temp of antenna in Kelvin
+        Tlna = 50; % Temp of LNA in Kelvin
+        Glna = 30; % Gain of the LNA in dB
+        Tmixer = 80; % Temp of the mixer in Kelvin
+        Gmixer = 0; % Gain of the mixer in dB
+   end
+   
+   methods
+       function output = GetSystemTemp(obj)
+           output = obj.Tant + obj.Tlna + obj.Tmixer/(10^(obj.Gmixer/10));
+       end
+   end
+end
---
+++ gsTrasmitSystem.m
@@ -0,0 +1,10 @@
+classdef gsTrasmitSystem
+    % The ground station that trasmits to the satellite
+   properties
+        % Phsyical properties of the system
+        Gtant = 60; % The transmit antenna directivity in dB
+   end
+   
+   methods
+   end
+end
